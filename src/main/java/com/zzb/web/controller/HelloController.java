650c306d3a18710fa28003f734ff2275a84c606d
Admin <developer@ainiding.com>
2020-09-03 17:52:37 +0800

    20200903001

---
+++ src/main/java/com/zzb/web/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.zzb.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author :zzb
+ * @createDate :2020/9/1 10:59
+ * @desc :
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
+    }
+
+}
